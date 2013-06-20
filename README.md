13bcbe2c3d3aedbbdf713860d9a1e18b77c32504
Derek Ries <influenztial@gmail.com>
2013-06-19 23:43:55 -0700

    highlighting works with normal filtering. will need adjusting for fuzzy filtering. paletteService allows for exporting commands from controllers to the palette

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
-Info coming soon.
+## angular-palette
+
+### What Is It?
+
+
+So I heard you liked the Sublime Text command palette. Me too. So much so, that 
+I thought it was missing from a lot of the web applications I build. 
+
+angular-palette is an angular component that aims to replicate the command
+palette from Sublime Text .
+
+**angular-palette** automatically takes all of the routes in your angularjs
+application and turns them into commands that can be executed from the command
+palette with the keyboard, suhweet! Sorry, I lied to you; It doesn't do it 
+'automatically'. You also need to include a name property on all of your routes
+in your $routeProvider configuration. This name property will be displayed in the
+palette.
+
+
+### Demo
+
+Just press 'ctrl+shift+m' to bring up the command palette. I'm not sure if that
+works in Mac or not. Probably not, I'll fix that. Works on Windows and Linux
+from what I've tested.
+
+### Why Should I Use It?
+
+Power users love to keep their hands on the keyboard at all times. Most web apps
+fall behind their desktop counterparts in this regard. (Note: angular-palette
+does not offer global keyboard shortcuts for commands. Maybe in the future)
+
+The palette works well for global actions across the application, routes and 
+links are the perfect candidate under this stance. You may also want to 
+expose various commands depending on the context of the app (current view). Or
+maybe you want to list out a collection of items in the palette. Those are both
+supported at the moment, but the API is pretty fragile and only supports exporting
+from one controller at a time (they replace each other) which is soon to be fixed.
+
+### Features
+ - Sublime Text Like Command Palette for quick actions with the keyboard
+ - Easily add all routes to the palette
+ - Export context-sensitive actions to the palette
+ - Looks Boss. Styled to look like the Dark Soda Theme
+
+### Coming Soon
+ - Multiple Styles (Light Soda first)
+ - Fuzzy Filtering just like sublime does. (Only normal filtering at the moment)
+ - Export commands from more than one controller at a time.
+ - Docs
+
--- palette.js
+++ palette.js
@@ -1,28 +1,52 @@
 'use strict';
 
 angular.module('palette', ['ngSanitize'])
-  .factory('paletteService', [function(){
-    return this;
+  .factory('paletteService', [function (){
+
+    var oldCommands = [];
+
+    return {
+
+      subscribedMethod: undefined,
+
+      exportCommands: function (newCommands) {
+        if(typeof this.subscribedMethod !== 'undefined'){
+          console.log('Should be calling subscribedMethod right now');
+          this.subscribedMethod(newCommands, oldCommands);
+        }
+        oldCommands = newCommands;
+      },
+      getCommands: function () {
+        return oldCommands;
+      },
+      subscribe: function (fn) {
+        console.log('Setting up subscribe ', fn);
+        this.subscribedMethod = fn;
+      }
+    };
+
   }])
-  .directive('ngBlur', function() {
+  .directive('ngBlur', function () {
     return function(scope, elem, attrs) {
-      elem.bind('blur', function(){
+      elem.bind('blur', function () {
         scope.$apply(attrs.ngBlur);
       });
     };
   })
-  .directive('focusOn', function() {
+  .directive('focusOn', [function () {
     return {
       restrict: 'A',
       link: function(scope, elem, attrs) {
         attrs.$observe('focusOn', function (newValue) {
           if(newValue === 'true'){
-            elem[0].focus();
+            setTimeout(function () {
+              elem[0].focus();
+            }, 100);
           }
         });
       }
     };
-  })
+  }])
   .directive('scrollToContain', function () {
     return {
       restrict: 'A',
@@ -36,32 +60,53 @@ angular.module('palette', ['ngSanitize'])
       }
     };
   })
-  .filter('highlight', function() {
+  .filter('highlight', function () {
 
-    function wrapText (index, str, prefix, suffix) {
-      return [str.slice(0, index), prefix, str.charAt(index), suffix, str.slice(index + 1)].join('');
+    function wrapText (index, str, prefix, suffix, innerTextLength) {
+      return [str.slice(0, index), prefix, str.slice(index, index + innerTextLength), suffix, str.slice(index + innerTextLength)].join('');
     }
 
     return function (value, query) {
       if(typeof query !== 'undefined' && query !== '') {
         var ind = value.toLowerCase().indexOf(query);
         if(ind !== -1){
-          return wrapText(ind, value, '<span class="palettematch">', '</span>');
+          return wrapText(ind, value, '<span class="palettematch">', '</span>', query.length);
         }
-        console.log(value, query, ind);
+        // console.log(value, query, ind);
       }
       return value;
     };
   })
-  .directive('palette', ['$timeout','$location', '$route', function($timeout, $location, $route){
+  .directive('palette', ['$timeout','$location', '$route', 'paletteService',
+    function($timeout, $location, $route, paletteService){
     return {
       restrict: 'E',
       replace: true,
       scope: {},
       templateUrl: 'drcomponents/palette/palette.tpl.html',
-      link: function (scope, elem) {
-        console.log('palette linker function', elem[0]);
+
+      link: function (scope) {
+        var keys = {};
+        angular.element(window).bind('keydown', function (e) {
+          keys[e.keyCode] = true;
+          if(keys[17] && keys[77]) {
+            if(scope.visible){
+              scope.$apply(function () {
+                scope.close();
+              });
+            }
+            else{
+              scope.$apply(function () {
+                scope.open();
+              });
+            }
+          }
+        })
+        .bind('keyup', function (e) {
+          keys[e.keyCode] = false;
+        });
       },
+
       controller: function ($scope) {
 
         var ENTER_KEY = 13,
@@ -69,23 +114,23 @@ angular.module('palette', ['ngSanitize'])
             DOWN_ARROW_KEY = 40,
             ESCAPE_KEY = 27;
 
-        $scope.visible = true;
+        $scope.visible = false;
         $scope.commands = [
-          { name: 'Soda Dark.sublime-theme' },
-          { name: 'Google.com' },
-          { name: 'Command Palette' },
-          { name: 'Sublime Text' },
-          { name: 'Bookmarks: Some weird bookmark' },
-          { name: 'Bookmarks: Angularjs components' },
-          { name: 'Bookmarks: Hey Dere', cmd: 'testerFunc' },
-          { name: 'Bookmarks: Portfolio Project' },
-          { name: 'Command Palette' },
+          { name: 'Fake: Placeholder Command' },
+          { name: 'Placeholder: Does Nothing' },
+          { name: 'Fake: Could Do Something. But I Doubt It.' },
+          { name: 'Placeholder: Just Try It Anyway' },
+          { name: 'Fake: Lorem ipsum dolor sit.' },
+          { name: 'Placeholder: Lorem ipsum dolor.' },
+          { name: 'Fake: Lorem ipsum dolor sit amet, consectetur.' },
+          { name: 'Placeholder: Lorem ipsum dolor sit amet.' },
+          { name: 'GOTO: Google.com', cmd: 'extLink', data: 'http://google.com' },
         ];
-        $scope.activeCmd = 0;
 
         function addRoutesToPallete () {
           for(var path in $route.routes){
             var route = $route.routes[path];
+
             if (typeof route.name !== 'undefined') {
               $scope.commands.push({
                 name: 'GOTO: ' + route.name,
@@ -96,8 +141,23 @@ angular.module('palette', ['ngSanitize'])
           }
         }
 
-        addRoutesToPallete();
+        function removeOldCommands (oldCommands) {
+          console.log('removed ', $scope.commands.splice(-oldCommands.length, oldCommands.length));
+        }
 
+        function addNewCommands (newCommands) {
+          $scope.commands.push.apply($scope.commands, newCommands);
+        }
+
+        paletteService.subscribe(function (newCommands, oldCommands) {
+          console.log('Change in the subscribe', oldCommands);
+          removeOldCommands(oldCommands);
+          addNewCommands(newCommands);
+        });
+
+        $scope.activeCmd = 0;
+        addRoutesToPallete();
+        // $scope.commands.push.apply($scope.commands, paletteService.getCommands());
 
         $scope.keyHandler = function (e) {
           if(e.keyCode === UP_ARROW_KEY) {
@@ -119,52 +179,6 @@ angular.module('palette', ['ngSanitize'])
           }
         };
 
-        $scope.finish = function () {
-          if($scope.visible){
-            $scope.useSelection($scope.filteredCommands[$scope.activeCmd]);
-            $scope.close();
-          }
-        };
-
-        $scope.useSelection = function (selection) {
-          if(typeof selection !== 'undefined') {
-            try {
-              $scope[selection.cmd](selection.data);
-            }
-            catch (e) {
-              console.log('missing a command');
-            }
-          }
-          else {
-            $scope.parseTextCommand($scope.query);
-          }
-        };
-
-        $scope.link = function (path) {
-          $location.path(path);
-        };
-
-        $scope.parseTextCommand = function (query) {
-          // Text commands are built into angular-palette
-          if(query[0] === ':') {
-            var newQuery = parseInt(query.slice(1), 10);
-            window.scrollTo(0, newQuery);
-          }
-        };
-
-        $scope.close = function () {
-          $scope.visible = false;
-          $timeout(function(){
-            $scope.open();
-          }, 1000);
-        };
-
-        $scope.open = function () {
-          $scope.query = '';
-          $scope.visible = true;
-          $scope.activeCmd = 0;
-        };
-
         $scope.moveSelectUp = function () {
           if($scope.activeCmd > 0) {
             $scope.activeCmd--;
@@ -183,8 +197,61 @@ angular.module('palette', ['ngSanitize'])
           }
         };
 
-        $scope.scrollToContain = function (item) {
+        $scope.finish = function () {
+          if($scope.visible){
+            $scope.useSelection($scope.filteredCommands[$scope.activeCmd]);
+            $scope.close();
+          }
+        };
 
+        $scope.close = function () {
+          // if this isn't delayed then the palette closes before ng-clicks can fire
+          // this enables the mouse to be used to select results (but don't use the mouse you bad)
+          $timeout(function () {
+            $scope.visible = false;
+          },1);
+        };
+
+        $scope.open = function () {
+          $scope.query = '';
+          $scope.visible = true;
+          $scope.activeCmd = 0;
+        };
+
+        $scope.useSelection = function (selection) {
+          if(typeof selection !== 'undefined') {
+            try {
+              if (typeof selection.cmd === 'function') {
+                selection.cmd(selection.data);
+              }
+              else {
+                $scope[selection.cmd](selection.data);
+              }
+            }
+            catch (e) {
+              console.log('missing a command');
+            }
+          }
+          else {
+            // Special Text Commands like ':300' to go 300px down
+            // More features will be implemented later
+            $scope.parseTextCommand($scope.query);
+          }
+        };
+
+        $scope.link = function (path) {
+          $location.path(path);
+        };
+
+        $scope.extLink = function (path) {
+          window.location = path;
+        };
+
+        $scope.parseTextCommand = function (query) {
+          if(query[0] === ':') {
+            var newQuery = parseInt(query.slice(1), 10);
+            window.scrollTo(0, newQuery);
+          }
         };
 
       }
